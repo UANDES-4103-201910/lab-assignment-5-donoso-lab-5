5e3eb8d6a10e3caddbd333d9ae0bde3446c7833c
JOhnny <jadonoso2@miuandes.cl>
2019-04-10 09:27:38 -0400

    COntrollers creados

---
+++ app/controllers/ticket_zones_controller.rb
@@ -0,0 +1,2 @@
+class TicketZonesController < ApplicationController
+end
---
+++ test/controllers/ticket_zones_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class TicketZonesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
